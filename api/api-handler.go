c419fff2481794138d576f5b21bae8070297bdd6
Ritwick Dey <ritwickdey@outlook.com>
2020-04-18 19:04:50 +0530

    date range api

--- api/api-handler.go
+++ api/api-handler.go
@@ -1,10 +1,15 @@
 package api
 
-import "github.com/ritwickdey/covid-19-india-golang/model"
+import (
+	"time"
+
+	"github.com/ritwickdey/covid-19-india-golang/model"
+)
 
 type Service interface {
 	FetchAllData() (model.Covid19StatMapDateWise, error)
 	FetchByDate(date string) (model.Covid19StatMap, error)
+	FetchByDateRange(from string, to string) (model.Covid19StatMapDateWise, error)
 }
 
 func NewService() Service {
@@ -20,4 +25,31 @@ func (s *service) FetchAllData() (model.Covid19StatMapDateWise, error) {
 
 func (s *service) FetchByDate(date string) (model.Covid19StatMap, error) {
 	return model.DataCache.GetCache()[date], nil
+}
+
+func (s *service) FetchByDateRange(fromStr string, toStr string) (model.Covid19StatMapDateWise, error) {
+	result := model.Covid19StatMapDateWise{}
+	from, err := time.Parse("02-01-2006", fromStr)
+
+	if err != nil {
+		return result, err
+	}
+
+	to, err := time.Parse("02-01-2006", toStr)
+
+	if err != nil {
+		return result, err
+	}
+
+	for i := from; to.Sub(i).Hours() >= 0; i = i.Add(time.Hour * 24) {
+		d := i.Format("02-01-2006")
+		covid19StatMap, err := s.FetchByDate(d)
+		if err != nil {
+			return result, err
+		}
+		result[d] = covid19StatMap
+	}
+
+	return result, nil
+
 }
--- api/transport.go
+++ api/transport.go
@@ -24,6 +24,13 @@ func MakeGetStatsByDateEndpoint(s Service) endpoint.Endpoint {
 	}
 }
 
+func MakeGetStatsByDateRangeEndpoint(s Service) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		var dates = request.([]string)
+		return s.FetchByDateRange(dates[0], dates[1])
+	}
+}
+
 func DecodeGetAllDataReq(_ context.Context, r *http.Request) (interface{}, error) {
 	return nil, nil
 }
@@ -36,6 +43,20 @@ func DecodeGetStatsByDateReq(_ context.Context, r *http.Request) (interface{}, e
 	}
 
 	return date, nil
+}
+
+func DecodeGetStatsByDateRangeReq(_ context.Context, r *http.Request) (interface{}, error) {
+	vars := mux.Vars(r)
+	startDate := vars["startDate"]
+	endDate := vars["endDate"]
+	if startDate == "" {
+		return nil, errors.New("start date is missing")
+	}
+	if endDate == "" {
+		return nil, errors.New("end date is missing")
+	}
+	dates := []string{startDate, endDate}
+	return dates, nil
 }
 
 func EncodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) (err error) {
@@ -59,6 +80,14 @@ func MakeHTTPHandler(s Service) http.Handler {
 	r.Methods("GET").Path("/covid19/date/{date}").Handler(
 		httptransport.NewServer(MakeGetStatsByDateEndpoint(s),
 			DecodeGetStatsByDateReq,
+			EncodeResponse,
+			options...,
+		),
+	)
+
+	r.Methods("GET").Path("/covid19/dateRange/{startDate}/{endDate}").Handler(
+		httptransport.NewServer(MakeGetStatsByDateRangeEndpoint(s),
+			DecodeGetStatsByDateRangeReq,
 			EncodeResponse,
 			options...,
 		),
